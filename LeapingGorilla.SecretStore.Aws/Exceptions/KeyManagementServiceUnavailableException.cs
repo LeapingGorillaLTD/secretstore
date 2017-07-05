43f588195068f81edf15d30a18a30a47b5dfa2c3
Gary H <17553415+gary-lg@users.noreply.github.com>
2017-07-05 17:34:00 +0100

    Update packages to latest versions

--- LeapingGorilla.SecretStore.Aws/Exceptions/KeyManagementServiceUnavailableException.cs
+++ LeapingGorilla.SecretStore.Aws/Exceptions/KeyManagementServiceUnavailableException.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Net;
+﻿using System.Net;
 using LeapingGorilla.SecretStore.Exceptions;
 
 namespace LeapingGorilla.SecretStore.Aws.Exceptions
 {
 	public class KeyManagementServiceUnavailableException : SecretStoreException
 	{
-		public HttpStatusCode? LastStatusCode { get; private set; }
+		public HttpStatusCode? LastStatusCode { get; }
 
-		public KeyManagementServiceUnavailableException(HttpStatusCode? lastStatusCode) : base($"Failed to contact the Key Management Service. Last status code: '{lastStatusCode}'. See http://docs.aws.amazon.com/kms/latest/APIReference/API_Decrypt.html for failed status code meanings")
+		public KeyManagementServiceUnavailableException(HttpStatusCode? lastStatusCode) 
+			: base($"Failed to contact the Key Management Service. Last status code: '{lastStatusCode}'. See http://docs.aws.amazon.com/kms/latest/APIReference/API_Decrypt.html for failed status code meanings")
 		{
 			LastStatusCode = lastStatusCode;
 		}
