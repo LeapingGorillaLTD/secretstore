1450936548864a4a0351187142a996acf9f0a007
Gary H <17553415+gary-lg@users.noreply.github.com>
2019-01-08 18:31:54 +0000

    Create NuGet package on Build

--- packageall.bat
+++ packageall.bat
@@ -1,6 +1,5 @@
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe LeapingGorilla.SecretStore\LeapingGorilla.SecretStore.csproj /t:Build;Package /p:Configuration="Release"
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe LeapingGorilla.SecretStore.Aws\LeapingGorilla.SecretStore.Aws.csproj /t:Build;Package /p:Configuration="Release"
+dotnet build -c Release LeapingGorilla.SecretStore.sln
 mkdir NuGet
 del /F /Q NuGet\*.*
-copy LeapingGorilla.SecretStore.Aws\NuGet\*.* NuGet
-copy LeapingGorilla.SecretStore\NuGet\*.* NuGet
+copy LeapingGorilla.SecretStore.Aws\bin\Release\*.nupkg NuGet
+copy LeapingGorilla.SecretStore\bin\Release\*.nupkg NuGet
